95ed86cd207c5e735047a65d1796869f76448b84
brian <muriithibrian283@gmail.com>
2024-10-28 14:14:05 +0300

    Fix error on declarations

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,11 +8,11 @@
 
 void selection_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
-		return;
-
 	size_t i, j, min;
 	int temp;
+
+	if (array == NULL || size < 2)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
